3fd9d1391d58de4c4d3bebf8b34d740cd7726a0d
Rafael Franco dos Santos <rafael.apfsantos@outlook.com>
2023-03-03 12:13:10 -0300

    feat: add script sql to get customers  with oorde details

---
+++ get-customers.sql
@@ -0,0 +1,11 @@
+﻿SELECT CONCAT(c.FirstName, ' ', c.LastName) AS FullName,
+       c.Age,
+       o.OrderID,
+       o.DateCreated,
+       o.MethodOfPurchase AS PurchaseMethod,
+       od.ProductNumber,
+       od.ProductOrigin
+FROM Customers c
+INNER JOIN Orders o ON c.PersonID = o.PersonID
+INNER JOIN OrderDetails od ON o.OrderID = od.OrderID
+WHERE od.ProductID = 1112222333
