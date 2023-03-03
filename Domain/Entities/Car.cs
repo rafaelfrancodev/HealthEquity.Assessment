9dcc2f7010c0dfd1e917ffc47d00784ec772c838
Rafael Franco dos Santos <rafael.apfsantos@outlook.com>
2023-03-03 10:44:52 -0300

    Add project files.

---
+++ Domain/Entities/Car.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain.Entities
+{
+    public class Car
+    {
+
+        public int Id { get; set; }
+        public string Make { get; set; }
+        public int Year { get; set; }
+        public int Doors { get; set; }
+        public string Color { get; set; }
+        public decimal Price { get; set; }
+    }
+}
